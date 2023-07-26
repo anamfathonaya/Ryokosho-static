6533d66315bc1e768d11d80ca74c781413370637
anamfathonaya <75225863+anamfathonaya@users.noreply.github.com>
2023-07-26 10:48:49 +0700

    Ryokosho-fetch

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#ca1f1f',
+        secondary: '#2ECCB2',
+        background: '#fde9cd',
+        hitam: '#050505',
+        accent: '#ff560e',
+      }
+    },
+  },
+  plugins: [],
+}
+
