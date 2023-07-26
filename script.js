3e502265a323fc85c1ac18893b2af0b06196eccd
anamfathonaya <75225863+anamfathonaya@users.noreply.github.com>
2023-07-26 14:07:45 +0700

    ookk

---
+++ script.js
@@ -0,0 +1,76 @@
+// SKRIP UNTUK WELCOME
+const texts = [
+  "Halo, Selamat Datang!",
+  "Hello, Welcome!",
+  "いらっしゃいませ!",
+  "환영 합니다!",
+];
+
+let textIndex = 0;
+const textElement = document.getElementById("typed-text");
+
+function typeText() {
+  const currentText = texts[textIndex];
+  let i = 0;
+  textElement.innerHTML = "";
+
+  function typeCharacter() {
+    if (i < currentText.length) {
+      textElement.innerHTML += currentText.charAt(i);
+      i++;
+      setTimeout(typeCharacter, 100); // Kecepatan pengetikan (ms)
+    } else {
+      setTimeout(eraseText, 1500); // Waktu tampilan teks sebelum dihapus (ms)
+    }
+  }
+
+  function eraseText() {
+    if (i >= 0) {
+      const newText = currentText.substring(0, i);
+      textElement.innerHTML = newText;
+      i--;
+      setTimeout(eraseText, 50); // Kecepatan penghapusan teks (ms)
+    } else {
+      textIndex = (textIndex + 1) % texts.length;
+      setTimeout(typeText, 500); // Waktu tampilan teks sebelum diganti (ms)
+    }
+  }
+
+  typeCharacter();
+}
+
+// Panggil fungsi untuk pertama kali
+typeText();
+
+// SKRIP untuk HAMBURGER
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// SKRIP untuk NAVBAR_SCRIPT
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Skrip Scroll
+menubar.onscroll = function () {
+  const menuBar = document.querySelector("#nav-menu");
+  const navOpen = menuBar.offsetTop;
+
+  if (menubar.scrollY > navOpen) {
+    menuBar.classList.add("navbar-opened");
+  } else {
+    menuBar.classList.remove("navbar-opened");
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#ca1f1f',
-        secondary: '#2ECCB2',
-        background: '#fde9cd',
-        hitam: '#050505',
-        accent: '#ff560e',
-      }
+        primary: "#ca1f1f",
+        secondary: "#2ECCB2",
+        background: "#fde9cd",
+        hitam: "#050505",
+        accent: "#ff560e",
+      },
     },
   },
   plugins: [],
-}
-
+};
